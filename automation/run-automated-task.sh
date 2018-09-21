ec3cac4d722efd0028856df5bbdef4d7c37ed5f8
David Adams <dcadams@stanford.edu>
2018-09-21 10:12:49 -0700

    Turn off collect.metrics.

--- automation/run-automated-task.sh
+++ automation/run-automated-task.sh
@@ -41,6 +41,3 @@ VIRTUALENV_EXTRA_ARGS="${VIRTUALENV_EXTRA_ARGS:-}"
 remote-task --job-flow-name="$CLUSTER_NAME" --repo $TASKS_REPO --branch $TASKS_BRANCH --wait --log-path $WORKSPACE/logs/ --remote-name automation --user $TASK_USER --virtualenv-extra-args="$VIRTUALENV_EXTRA_ARGS" --secure-config-branch="$SECURE_BRANCH" --secure-config-repo="$SECURE_REPO" --secure-config="$SECURE_CONFIG" --override-config="$OVERRIDE_CONFIG" "$@"
 
 cat $WORKSPACE/logs/* || true
-
-. $CONF_BIN/activate
-make -C analytics-configuration collect.metrics
